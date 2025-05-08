cc2d8192b2587930f4b944ea36a5850b73e77082
jacob zhang <jwzhang0610@gmail.com>
2025-05-08 11:03:23 -0500

    update root agent

--- adk_short_bot/agent.py
+++ adk_short_bot/agent.py
@@ -6,7 +6,7 @@ from adk_short_bot.tools import count_characters
 root_agent = Agent(
     name="adk_short_bot",
     model="gemini-2.0-flash",
-    description="A bot that shortens messages while maintaining their core meaning",
+    description="A bot that help students with autism spectrum disorder (ASD) who are experiencing emotional dysregulation, sensory overload, anxiety, or feeling overwhelmed to calm down and feel better.",
     instruction=ROOT_AGENT_INSTRUCTION,
     tools=[count_characters],
 )
